a705c090716e0a2318c083feab6c2e37a522579b
Rosa Elvia Cruz Vazquz <rosaecv.96@gmail.com>
2017-03-13 21:58:19 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,15 @@
+<?php
+//comentarios
+#comentarios
+/*comentarios
+multiliena*/
+echo "Hola Mundo php";
+?>
+<html>
+<head>
+<title>practica 10 php </title>
+</head>
+<body>
+<h1> Hola Mundo html</h1>
+</body>
+</html>
---
+++ practica10/index2.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>practica 10 php </title>
+</head>
+<body>
+   <h1> usando PHP</h1>
+   <p><?php
+   echo "parrafo php";
+   ?></p>
+   <p> Parrafo HTML</p>
+   <h3>
+   <?php
+   echo "parrafo php";
+   ?>
+   </h3>
+</body>
+</html>
